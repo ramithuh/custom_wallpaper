b31fb038643fbfa4b81901b9cdc5b8364622d316
Ramith Hettiarachchi <ramith@cmu.edu>
2026-01-03 05:05:07 -0500

    Change shuffling interval back to 15 minutes

--- src/app/api/wallpaper/route.tsx
+++ src/app/api/wallpaper/route.tsx
@@ -52,7 +52,7 @@ export async function GET(req: NextRequest) {
         now = new Date();
     }
     const minutes = now.getMinutes();
-    const shufflingInterval = 10;
+    const shufflingInterval = 15;
     let viewIndex = Math.floor(minutes / shufflingInterval) % 3;
 
     // Override with view parameter if provided
